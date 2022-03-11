8d736b8a777aca5c21b834e6f7620f65abaed3ca
adamwoolhether <adamwoolhether@gmail.com>
2022-03-11 08:34:17 +0800

    added support for double token operators, peakChar to view next char

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -46,13 +46,27 @@ func (l *Lexer) NextToken() token.Token {
 	
 	switch l.ch {
 	case '=':
-		tok = newToken(token.ASSIGN, l.ch)
+		if l.peekChar() == '=' {
+			ch := l.ch
+			l.readChar()
+			literal := string(ch) + string(l.ch)
+			tok = token.Token{Type: token.EQ, Literal: literal}
+		} else {
+			tok = newToken(token.ASSIGN, l.ch)
+		}
 	case '+':
 		tok = newToken(token.PLUS, l.ch)
 	case '-':
 		tok = newToken(token.MINUS, l.ch)
 	case '!':
-		tok = newToken(token.BANG, l.ch)
+		if l.peekChar() == '=' {
+			ch := l.ch
+			l.readChar()
+			literal := string(ch) + string(l.ch)
+			tok = token.Token{Type: token.NOT_EQ, Literal: literal}
+		} else {
+			tok = newToken(token.BANG, l.ch)
+		}
 	case '/':
 		tok = newToken(token.SLASH, l.ch)
 	case '*':
@@ -144,4 +158,16 @@ func (l *Lexer) readNumber() string {
 // isDigit checks whether the passed byte is a digit between 0 and 9.
 func isDigit(ch byte) bool {
 	return '0' <= ch && ch <= '9'
+}
+
+// peakChar is similar to readChar(), but doesn't increment the l.position.
+// It simply returns the next character, which is necessary for the lexer
+// to make decisions when a token is composed of two characters. Complex
+// languages require peeking further ahead, and sometimes backwards.
+func (l *Lexer) peekChar() byte {
+	if l.readPosition >= len(l.input) {
+		return 0
+	} else {
+		return l.input[l.readPosition]
+	}
 }
