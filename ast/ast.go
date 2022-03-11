ffcc1e6065b114170325dcf1fc07f880f2fc79c2
adamwoolhether <adamwoolhether@gmail.com>
2022-03-11 22:57:54 +0800

    added returnStatements to parser and AST

--- ast/ast.go
+++ ast/ast.go
@@ -71,3 +71,15 @@ func (i *Identifier) expressionNode() {}
 func (i *Identifier) TokenLiteral() string {
 	return i.Token.Literal
 }
+
+// ReturnStatement represents a return statement in Monkey.
+// It's methods satisfy the Statement and Node interfaces.
+type ReturnStatement struct {
+	Token       token.Token // the 'return' token
+	ReturnValue Expression
+}
+
+func (rs *ReturnStatement) statementNode() {}
+func (rs *ReturnStatement) TokenLiteral() string {
+	return rs.Token.Literal
+}
--- parser/parser.go
+++ parser/parser.go
@@ -76,6 +76,8 @@ func (p *Parser) parseStatement() ast.Statement {
 	switch p.curToken.Type {
 	case token.LET:
 		return p.parseLetStatement()
+	case token.RETURN:
+		return p.parseReturnStatement()
 	default:
 		return nil
 	}
@@ -127,4 +129,20 @@ func (p *Parser) peekTokenIs(t token.TokenType) bool {
 
 func (p *Parser) curTokenIs(t token.TokenType) bool {
 	return p.curToken.Type == t
+}
+
+// parseReturnStatment constructs an *ast.Statement node with
+// the current token, skipping over until it encounters a
+// semicolon.
+func (p *Parser) parseReturnStatement() *ast.ReturnStatement {
+	stmt := &ast.ReturnStatement{Token: p.curToken}
+	
+	p.nextToken()
+	
+	// TODO: we're skipping expressions until we encounter a semicolon.
+	for !p.curTokenIs(token.SEMICOLON) {
+		p.nextToken()
+	}
+	
+	return stmt
 }
