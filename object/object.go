c02bc53d2948c78157f1ea1350c87401ed6bf87e
adamwoolhether <adamwoolhether@gmail.com>
2022-03-16 15:23:18 +0800

    implement integer, boolean, and null types in object package

--- object/object.go
+++ object/object.go
@@ -3,10 +3,42 @@
 // are represented as an Object.
 package object
 
+import "fmt"
+
 type ObjectType string
 
-// Object defines the contract for all values in Monkey
+const (
+	INTEGER_OBJ = "INTEGER"
+	BOOLEAN_OBJ = "BOOLEAN"
+	NULL_OBJ    = "NULL"
+)
+
+// Object defines the contract for all values in Monkey.
 type Object interface {
 	Type() ObjectType
 	Inspect() string
 }
+
+// Integer holds the value of
+// ast.IntegerLiteral objects.
+type Integer struct {
+	Value int64
+}
+
+func (i *Integer) Type() ObjectType { return INTEGER_OBJ }
+func (i *Integer) Inspect() string  { return fmt.Sprintf("%d", i.Value) }
+
+// Boolean holds the value of
+// ast.Boolean objects
+type Boolean struct {
+	Value bool
+}
+
+func (b *Boolean) Type() ObjectType { return BOOLEAN_OBJ }
+func (b *Boolean) Inspect() string  { return fmt.Sprintf("%t", b.Value) }
+
+// Null represents the absence of any value.
+type Null struct{}
+
+func (n *Null) Type() ObjectType { return NULL_OBJ }
+func (n *Null) Inspect() string  { return "null" }
