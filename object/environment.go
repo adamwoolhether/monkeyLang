a76acf83888f17a54934e6a7450c6d11c1296c72
adamwoolhether <adamwoolhether@gmail.com>
2022-03-23 11:02:20 +0800

    functions and function calls

--- object/environment.go
+++ object/environment.go
@@ -1,19 +1,36 @@
 package object
 
 // Environment keeps track of values by associating
-// them to an Object with a name.
+// them to an Object with a name. The outer field
+// contains a reference to another object.Environment,
+// which mirrors how variable scopes are perceived.
 type Environment struct {
 	store map[string]Object
+	outer *Environment
+}
+
+// NewEnclosedEnvironment allows extending an environment by
+// creating a new Environment with a pointer to the enclosing
+// environment that it extends. This allows preserving
+// previous bindings of a function call.
+func NewEnclosedEnvironment(outer *Environment) *Environment {
+	env := NewEnvironment()
+	env.outer = outer
+	
+	return env
 }
 
 // NewEnvironment returns a new *Environment
 func NewEnvironment() *Environment {
 	s := make(map[string]Object)
-	return &Environment{store: s}
+	return &Environment{store: s, outer: nil}
 }
 
 func (e *Environment) Get(name string) (Object, bool) {
 	obj, ok := e.store[name]
+	if !ok && e.outer != nil {
+		obj, ok = e.outer.Get(name)
+	}
 	return obj, ok
 }
 
--- object/object.go
+++ object/object.go
@@ -3,7 +3,13 @@
 // are represented as an Object.
 package object
 
-import "fmt"
+import (
+	"bytes"
+	"fmt"
+	"strings"
+	
+	"github.com/adamwoolhether/monkeyLang/ast"
+)
 
 type ObjectType string
 
@@ -13,6 +19,7 @@ const (
 	NULL_OBJ         = "NULL"
 	RETURN_VALUE_OBJ = "RETURN_VALUE"
 	ERROR_OBJ        = "ERROR"
+	FUNCTION_OBJ     = "FUNCTION"
 )
 
 // Object defines the contract for all values in Monkey.
@@ -63,3 +70,31 @@ type Error struct {
 
 func (e *Error) Type() ObjectType { return ERROR_OBJ }
 func (e *Error) Inspect() string  { return "ERROR: " + e.Message }
+
+// Function represents a Function internally, holding the function
+// Body, Parameters. It also has an Env field, beacuse monkey
+// functions carry their own environment, which allows for closures.
+type Function struct {
+	Parameters []*ast.Identifier
+	Body       *ast.BlockStatement
+	Env        *Environment
+}
+
+func (f *Function) Type() ObjectType { return FUNCTION_OBJ }
+func (f *Function) Inspect() string {
+	var out bytes.Buffer
+	
+	params := []string{}
+	for _, p := range f.Parameters {
+		params = append(params, p.String())
+	}
+	
+	out.WriteString("fn")
+	out.WriteString("(")
+	out.WriteString(strings.Join(params, ", "))
+	out.WriteString(") {\n}")
+	out.WriteString(f.Body.String())
+	out.WriteString("\n")
+	
+	return out.String()
+}
