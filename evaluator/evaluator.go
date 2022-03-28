184c041e11ab38c180434771471990f850ac77ac
adamwoolhether <adamwoolhether@gmail.com>
2022-03-28 18:15:36 +0800

    Arrays - implemented built-in functions for arrays

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -15,24 +15,6 @@ var (
 	FALSE = &object.Boolean{Value: false}
 )
 
-// builtins enables built-in funcs to be accessed.
-var builtins = map[string]*object.Builtin{
-	"len": &object.Builtin{
-		Fn: func(args ...object.Object) object.Object {
-			if len(args) != 1 {
-				return newError("wrong number of arguments. got=%d, want=1", len(args))
-			}
-			
-			switch arg := args[0].(type) {
-			case *object.String:
-				return &object.Integer{Value: int64(len(arg.Value))}
-			default:
-				return newError("arguments to `len` not supported, got %s", args[0].Type())
-			}
-		},
-	},
-}
-
 // isError is a helper funtion checking if an object is an error or not.
 func isError(obj object.Object) bool {
 	if obj == nil {
