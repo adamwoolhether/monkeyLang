048fc40c6968cfc9d9ca75c4208ba53ba242d2bf
adamwoolhether <adamwoolhether@gmail.com>
2022-03-28 10:07:49 +0800

    Functions - built-in LEN func implemented

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -15,6 +15,24 @@ var (
 	FALSE = &object.Boolean{Value: false}
 )
 
+// builtins enables built-in funcs to be accessed.
+var builtins = map[string]*object.Builtin{
+	"len": &object.Builtin{
+		Fn: func(args ...object.Object) object.Object {
+			if len(args) != 1 {
+				return newError("wrong number of arguments. got=%d, want=1", len(args))
+			}
+			
+			switch arg := args[0].(type) {
+			case *object.String:
+				return &object.Integer{Value: int64(len(arg.Value))}
+			default:
+				return newError("arguments to `len` not supported, got %s", args[0].Type())
+			}
+		},
+	},
+}
+
 // isError is a helper funtion checking if an object is an error or not.
 func isError(obj object.Object) bool {
 	if obj == nil {
@@ -248,12 +266,15 @@ func newError(format string, a ...interface{}) *object.Error {
 }
 
 func evalIdentifier(node *ast.Identifier, env *object.Environment) object.Object {
-	val, ok := env.Get(node.Value)
-	if !ok {
-		return newError("identifier not found: " + node.Value)
+	if val, ok := env.Get(node.Value); ok {
+		return val
 	}
 	
-	return val
+	if builtin, ok := builtins[node.Value]; ok {
+		return builtin
+	}
+	
+	return newError("identifier not found: " + node.Value)
 }
 
 func evalExpressions(exps []ast.Expression, env *object.Environment) []object.Object {
@@ -270,15 +291,16 @@ func evalExpressions(exps []ast.Expression, env *object.Environment) []object.Ob
 }
 
 func applyFunction(fn object.Object, args []object.Object) object.Object {
-	function, ok := fn.(*object.Function)
-	if !ok {
+	switch fn := fn.(type) {
+	case *object.Function:
+		extendedEnv := extendFunctionEnv(fn, args)
+		evaluated := Eval(fn.Body, extendedEnv)
+		return unwrapReturnValue(evaluated)
+	case *object.Builtin:
+		return fn.Fn(args...)
+	default:
 		return newError("not a function: %s", fn.Type())
 	}
-	
-	extendedEnv := extendFunctionEnv(function, args)
-	evaluated := Eval(function.Body, extendedEnv)
-	
-	return unwrapReturnValue(evaluated)
 }
 
 func extendFunctionEnv(fn *object.Function, args []object.Object) *object.Environment {
