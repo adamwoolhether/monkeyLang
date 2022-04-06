6b4270e9beff12a9e4bf69de49490d0b5383b206
adamwoolhether <adamwoolhether@gmail.com>
2022-04-06 11:18:45 +0800

    Hashes - Hashing literals parsing implemented

--- ast/ast.go
+++ ast/ast.go
@@ -370,3 +370,27 @@ func (ie *IndexExpression) String() string {
 	
 	return out.String()
 }
+
+// HashLiteral represents comma-separated list of key-value pairs.
+// it is represented by Gp's map structure.
+type HashLiteral struct {
+	Token token.Token // The '{' token
+	Pairs map[Expression]Expression
+}
+
+func (hl HashLiteral) expressionNode()      {}
+func (hl HashLiteral) TokenLiteral() string { return hl.Token.Literal }
+func (hl HashLiteral) String() string {
+	var out bytes.Buffer
+	
+	pairs := []string{}
+	for key, value := range hl.Pairs {
+		pairs = append(pairs, key.String()+":"+value.String())
+	}
+	
+	out.WriteString("{")
+	out.WriteString(strings.Join(pairs, ", "))
+	out.WriteString("}")
+	
+	return out.String()
+}
