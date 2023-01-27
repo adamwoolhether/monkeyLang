7cab5207adc6cfb86b44137ca0254f377af6888a
adamwoolhether <adamwoolhether@gmail.com>
2023-01-27 15:36:31 +0800

    implment build in functions into vm

--- vm/vm.go
+++ vm/vm.go
@@ -201,7 +201,7 @@ func (vm *VM) Run() error {
 			numArgs := code.ReadUint8(ins[ip+1:])
 			vm.currentFrame().ip += 1
 
-			if err := vm.callFunction(int(numArgs)); err != nil {
+			if err := vm.executeCall(int(numArgs)); err != nil {
 				return err
 			}
 		case code.OpReturnValue:
@@ -218,6 +218,15 @@ func (vm *VM) Run() error {
 			vm.sp = frame.basePointer - 1
 
 			if err := vm.push(Null); err != nil {
+				return err
+			}
+		case code.OpGetBuiltin:
+			builtinIndex := code.ReadUint8(ins[ip+1:])
+			vm.currentFrame().ip += 1
+
+			definition := object.Builtins[builtinIndex]
+
+			if err := vm.push(definition.Builtin); err != nil {
 				return err
 			}
 		}
@@ -433,6 +442,18 @@ func (vm *VM) executeHashIndex(hash, index object.Object) error {
 	return vm.push(pair.Value)
 }
 
+func (vm *VM) executeCall(numArgs int) error {
+	callee := vm.stack[vm.sp-1-numArgs]
+	switch callee := callee.(type) {
+	case *object.CompiledFunction:
+		return vm.callFunction(callee, numArgs)
+	case *object.Builtin:
+		return vm.callBuiltin(callee, numArgs)
+	default:
+		return fmt.Errorf("calling non-function an non-built-in")
+	}
+}
+
 // isTruthy determines if a stack element is truthy or not.
 func isTruthy(obj object.Object) bool {
 	switch o := obj.(type) {
@@ -489,19 +510,34 @@ func (vm *VM) popFrame() *Frame {
 	return vm.frames[vm.framesIndex]
 }
 
-func (vm *VM) callFunction(numArgs int) error {
-	fn, ok := vm.stack[vm.sp-1-int(numArgs)].(*object.CompiledFunction)
-	if !ok {
-		return fmt.Errorf("calling non-function")
-	}
-
+func (vm *VM) callFunction(fn *object.CompiledFunction, numArgs int) error {
 	if numArgs != fn.NumParameters {
 		return fmt.Errorf("wrong number of arguments: want=%d, got=%d", fn.NumParameters, numArgs)
 	}
 
 	frame := NewFrame(fn, vm.sp-numArgs)
 	vm.pushFrame(frame)
+
 	vm.sp = frame.basePointer + fn.NumLocals
+
+	return nil
+}
+
+func (vm *VM) callBuiltin(builtin *object.Builtin, numArgs int) error {
+	args := vm.stack[vm.sp-numArgs : vm.sp]
+
+	result := builtin.Fn(args...)
+	vm.sp = vm.sp - numArgs - 1
+
+	if result != nil {
+		if err := vm.push(result); err != nil {
+			return err
+		}
+	} else {
+		if err := vm.push(Null); err != nil {
+			return err
+		}
+	}
 
 	return nil
 }
